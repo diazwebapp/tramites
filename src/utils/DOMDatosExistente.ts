06c3d9a4e63697bbbac32f8997a8a77d8ed0fb95
diazwebapp <diazwebapp@gmail.com>
2026-05-10 17:02:13 +0000

    mejorando la ux

--- src/utils/DOMDatosExistente.ts
+++ src/utils/DOMDatosExistente.ts
@@ -3,7 +3,7 @@ import type { PersonData, VehicleData } from "../types";
 export const createDOMElement = (data:VehicleData | PersonData,DOMElement:HTMLElement)=>{
     for (const clave in data) {
         if (data.hasOwnProperty(clave)) { // Asegúrate de que la propiedad es propia del objeto
-          const valor = data[clave];
+          const valor = data[clave];          
 
           // Crea un párrafo para cada par clave-valor
           const parrafo = document.createElement('p');
